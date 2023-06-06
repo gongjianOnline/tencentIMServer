7e08343fa9f2dd66200f5671a24b3d99275d9735
“李承斌” <15031617367@163.com>
2023-06-06 17:35:56 +0800

    腾讯IM秘钥获取成功

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 const router = require('koa-router')()
+var TLSSigAPIv2 = require('tls-sig-api-v2');
 
-router.get('/', async (ctx, next) => {
+// 调用后台的login接口获取腾讯的 sign 秘钥
+router.post("/login",async (ctx,next)=>{
+  var {name} = ctx.request.body;
+  console.log(name);
+  var api = new TLSSigAPIv2.Api(1400813777, "5d447bbc2bea986a10e1bd1308bfb1aeaeed111dcf3211f7ca86d3368a5f3fc6 ");
+  var sig = api.genSig(name, 86400*180);
   ctx.body = {
     code:101,
     data:{
-      name:'张三'
+      sig:sig
     },
-    message:'返回成功'
-  }
+    message:"success"
+}
 })
+
+
 
 module.exports = router
