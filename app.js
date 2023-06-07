97f467884d2e52a409d1e39cc3bc679e5c155881
“李承斌” <15031617367@163.com>
2023-06-07 09:31:56 +0800

    后端项目中添加了swagger 和 热更新

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const {koaSwagger} = require('koa2-swagger-ui')
+
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+
+// swagger UI
+const swagger = require('./util/swagger')
 
 // error handler
 onerror(app)
@@ -35,6 +40,15 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+
+// swagger 
+app.use(koaSwagger({
+  routePrefix: '/swagger', // host at /swagger instead of default /docs
+  swaggerOptions: {
+    url: '/swagger.json', // example path to json 其实就是之后swagger-jsdoc生成的文档地址
+  },
+}))
+app.use(swagger.routes(), swagger.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,59 @@
 const router = require('koa-router')()
 var TLSSigAPIv2 = require('tls-sig-api-v2');
 
-// 调用后台的login接口获取腾讯的 sign 秘钥
+//  get方式
+// /**
+//  * @swagger
+//  * /login:
+//  *   get:
+//  *     tags:
+//  *       - IM登录
+//  *     summary: IM登录获取sign
+//  *     parameters:
+//  *       - name: name
+//  *         in: query
+//  *         required: true
+//  *         description: 用户名
+//  *         type: string
+//  *     responses:
+//  *      200: 
+//  *        description: 成功获取
+//  */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags:
+ *       - IM登录
+ *     summary: 用户登录
+ *     description: 用户通过用户名和密码进行登录
+ *     parameters:
+ *       - in: body
+ *         name: data
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *     responses:
+ *      200: 
+ *        description: 成功获取
+ */
 router.post("/login",async (ctx,next)=>{
   var {name} = ctx.request.body;
   console.log(name);
+  if(!name){
+    ctx.body = {
+      code:102,
+      data:{
+        sig:null
+      },
+      message:"用户名不能为空"
+    }
+    return
+  }
   var api = new TLSSigAPIv2.Api(1400813777, "5d447bbc2bea986a10e1bd1308bfb1aeaeed111dcf3211f7ca86d3368a5f3fc6 ");
   var sig = api.genSig(name, 86400*180);
   ctx.body = {
@@ -13,7 +62,7 @@ router.post("/login",async (ctx,next)=>{
       sig:sig
     },
     message:"success"
-}
+  }
 })
 
 
